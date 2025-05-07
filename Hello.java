e2c787ecd0be64a8ba8f3cce754783ee6fe067be
GustavooPerini <gustavooperini@gmail.com>
2025-05-07 16:39:46 -0300

    Leve acerto no comentario do arquivo .java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
-        //Isso printa uma mensagem
+        // Isso imprime uma mensagem
         System.out.println("Hello, World!");
     }
 }
